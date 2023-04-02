6bdca8f555b64e3164c955dead2c0829592e974d
azizove <az.elouarak@gmail.com>
2023-04-02 09:10:46 -0400

    chore: make role dynamic

--- client/src/components/dice/DiceCubes.tsx
+++ client/src/components/dice/DiceCubes.tsx
@@ -1,16 +1,40 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactDice, { ReactDiceRef } from 'react-dice-complete'
+import json from '../../contracts/Monopoly.json';
+import useWeb3 from '../../hooks/web3';
+import { Contract } from 'web3-eth-contract';
+
 
 export default function DiceCubes() {
   const reactDice = useRef<ReactDiceRef>(null)
+  // const { isLoading, isWeb3, web3, accounts } = useWeb3();
+  // const [instance, setInstance] = useState<Contract>();
+  // const [value, setValue] = useState('');
 
-  const rollDone = (totalValue: number, values: number[]) => {
-    console.log('individual die values array:', values)
-    console.log('total dice value:', totalValue)
+  // const abi: any = json.abi;
+
+  // useEffect(() => {
+  //   (async() => {
+  //     if(web3 !== null) {
+  //       // const networkId = await web3.eth.net.getId();
+  //       const deployedNetwork = json.networks;
+  //       const instance = new web3.eth.Contract(
+  //         abi,
+  //         deployedNetwork && deployedNetwork.address
+  //       );
+  //       setInstance(instance);
+  //     }
+  //   })();
+  // }, [isLoading, isWeb3]);
+  let rollDone = (totalValue: number, values: number[]) => {
+    console.log(reactDice.current)
   }
 
   const rollAll = () => {
-    reactDice.current?.rollAll()
+    // TODO: Get dice results from smart contract
+    const dataFromSmartContract = [5, 2]
+
+    reactDice.current?.rollAll(dataFromSmartContract)
   }
 
     return (
