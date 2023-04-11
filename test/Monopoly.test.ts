1b282b96c0f75172f9112998ae5fabbcbdc2f157
soushi888 <sacha.pignot@protonmail.com>
2023-04-10 23:40:21 -0400

    add MonopolyContract interface

--- test/Monopoly.test.ts
+++ test/Monopoly.test.ts
@@ -1,32 +1,61 @@
 import {expect, use} from 'chai';
-import {Contract} from 'ethers';
+import {Contract, Wallet} from 'ethers';
 import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
 import Monopoly from '../build/Monopoly.json';
 import MoneyPoly from '../build/MoneyPoly.json';
 
 use(solidity);
 
+interface MonopolyContract extends Contract {
+	assignPlayerNumber(): Promise<void>;
+
+	resetGame(): Promise<void>;
+
+	getPlayerAddress(playerNumber: number): Promise<string>;
+
+	getGameOn(): Promise<boolean>;
+
+	getPlayerTurn(): Promise<number>;
+
+	throwDice(): Promise<void>;
+
+	buildHouse(): Promise<void>;
+
+	endTurn(): Promise<void>;
+
+	getHouses(): Promise<number[][]>;
+
+	players(player: string): Promise<{ playerPosition: number; playerNumber: number }>;
+
+	properties(index: number): Promise<{ isConstructible: boolean; constructionCost: number; rentPrice: number }>;
+
+	houses(index: number): Promise<{ position: number; owner: number }>;
+}
+
+function connectMonopoly(contract: Contract, wallet: Wallet) {
+	return contract.connect(wallet) as MonopolyContract;
+}
+
 describe('Monopoly contract', () => {
-	const [player1, player2, player3, player4] = new MockProvider().getWallets();
-	let monopoly: Contract;
+	const [playerWallet1, playerWallet2, playerWallet3, playerWallet4] = new MockProvider().getWallets();
+	let monopoly: MonopolyContract;
 	let moneyPoly: Contract;
 
 	beforeEach(async () => {
-		moneyPoly = await deployContract(player1, MoneyPoly);
+		moneyPoly = await deployContract(playerWallet1, MoneyPoly);
 		let moneyPolyAddress = moneyPoly.address;
-		monopoly = await deployContract(player1, Monopoly, [player1.address, moneyPolyAddress]);
+		monopoly = await deployContract(playerWallet1, Monopoly, [playerWallet1.address, moneyPolyAddress]) as MonopolyContract;
 	});
 
 	it('should assign unique player numbers', async function () {
-		await monopoly.connect(player1).assignPlayerNumber();
-		await monopoly.connect(player2).assignPlayerNumber();
+		await connectMonopoly(monopoly, playerWallet1).assignPlayerNumber();
+		await connectMonopoly(monopoly, playerWallet2).assignPlayerNumber();
 
-		let player1Number = await monopoly.players(player1.address);
-		let player2Number = await monopoly.players(player2.address);
+		let player1 = await monopoly.players(playerWallet1.address);
+		let player2 = await monopoly.players(playerWallet2.address);
 
-		expect(player1Number).to.not.equal(0);
-		expect(player2Number).to.not.equal(0);
-		expect(player1Number).to.not.equal(player2Number);
+		expect(player1.playerNumber).to.equal(1);
+		expect(player2.playerNumber).to.equal(2);
 	});
 
 	// it('should change game status to true when 4 players are assigned', async function () {
