323154e71ce59de8831e44097cd441695d949963
azizove <az.elouarak@gmail.com>
2023-08-07 11:56:46 -0400

    fix remove strict mode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,11 +8,9 @@ import store from "./store";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById("root")
 );
 serviceWorker.unregister();
